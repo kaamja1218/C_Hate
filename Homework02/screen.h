151218c62c18845e37481a30af2284096e3cfb0c
kaamja1218 <127713897+kaamja1218@users.noreply.github.com>
2023-05-22 23:16:43 +0900

    Update screen.h

--- Homework02/screen.h
+++ Homework02/screen.h
@@ -1,7 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-/* 버퍼 초기화 함수 */
 int clearBuffer(char* screenBuf, int width, int height);
 
-/* 수업시간에 배운 x,y에 스트링을 쓰는 함수를 짜면 됨. */
 int writeStringToBuffer(const char* string, int x, int y);
